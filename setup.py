d5caa2fbcb5c065950ce236078fc3eeb54815be1
Nicholas Klem <nicholas@sanity.io>
2025-04-01 21:19:23 +0200

    chore: essential updates for Python compatibility

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='sops-pre-commit',
     description='Check for unencrypted Kubernetes secrets in manifest files',
     url='https://github.com/onedr0p/sops-pre-commit',
-    version='2.1.0',
+    version='2.1.1',
 
     author='Devin Buhl',
     author_email='devin.kray@gmail.com',
@@ -12,17 +12,17 @@ setup(
     platforms='linux',
     classifiers=[
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
 
     packages=find_packages('.'),
+    install_requires=['pyyaml>=5.1'],
     entry_points={
         'console_scripts': [
             'forbid_secrets = hooks.forbid_secrets:main',
